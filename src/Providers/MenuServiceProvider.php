0fd76b2b07996a594c0d03066f92fca61b9ea8b9
Konstantin Bukin <bukin@inetstudio.ru>
2018-07-29 05:29:39 +0300

    Defer bindings

--- src/Providers/MenuServiceProvider.php
+++ src/Providers/MenuServiceProvider.php
@@ -20,7 +20,6 @@ class MenuServiceProvider extends ServiceProvider
         $this->registerPublishes();
         $this->registerRoutes();
         $this->registerViews();
-        $this->registerObservers();
     }
 
     /**
@@ -86,66 +85,5 @@ class MenuServiceProvider extends ServiceProvider
     protected function registerViews(): void
     {
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'admin.module.menu');
-    }
-
-    /**
-     * Регистрация наблюдателей.
-     *
-     * @return void
-     */
-    public function registerObservers(): void
-    {
-        $this->app->make('InetStudio\Menu\Contracts\Models\MenuModelContract')::observe($this->app->make('InetStudio\Menu\Contracts\Observers\MenuObserverContract'));
-        $this->app->make('InetStudio\Menu\Contracts\Models\MenuItemModelContract')::observe($this->app->make('InetStudio\Menu\Contracts\Observers\MenuItemObserverContract'));
-    }
-
-    /**
-     * Регистрация привязок, алиасов и сторонних провайдеров сервисов.
-     *
-     * @return void
-     */
-    public function registerBindings(): void
-    {
-        // Controllers
-        $this->app->bind('InetStudio\Menu\Contracts\Http\Controllers\Back\MenusControllerContract', 'InetStudio\Menu\Http\Controllers\Back\MenusController');
-        $this->app->bind('InetStudio\Menu\Contracts\Http\Controllers\Back\MenusDataControllerContract', 'InetStudio\Menu\Http\Controllers\Back\MenusDataController');
-
-        // Events
-        $this->app->bind('InetStudio\Menu\Contracts\Events\Back\ModifyMenuEventContract', 'InetStudio\Menu\Events\Back\ModifyMenuEvent');
-
-        // Models
-        $this->app->bind('InetStudio\Menu\Contracts\Models\MenuModelContract', 'InetStudio\Menu\Models\MenuModel');
-        $this->app->bind('InetStudio\Menu\Contracts\Models\MenuItemModelContract', 'InetStudio\Menu\Models\MenuItemModel');
-
-        // Observers
-        $this->app->bind('InetStudio\Menu\Contracts\Observers\MenuObserverContract', 'InetStudio\Menu\Observers\MenuObserver');
-        $this->app->bind('InetStudio\Menu\Contracts\Observers\MenuItemObserverContract', 'InetStudio\Menu\Observers\MenuItemObserver');
-
-        // Repositories
-        $this->app->bind('InetStudio\Menu\Contracts\Repositories\MenusRepositoryContract', 'InetStudio\Menu\Repositories\MenusRepository');
-        $this->app->bind('InetStudio\Menu\Contracts\Repositories\MenuItemsRepositoryContract', 'InetStudio\Menu\Repositories\MenuItemsRepository');
-
-        // Requests
-        $this->app->bind('InetStudio\Menu\Contracts\Http\Requests\Back\SaveMenuRequestContract', 'InetStudio\Menu\Http\Requests\Back\SaveMenuRequest');
-
-        // Responses
-        $this->app->bind('InetStudio\Menu\Contracts\Http\Responses\Back\Menus\DestroyResponseContract', 'InetStudio\Menu\Http\Responses\Back\Menus\DestroyResponse');
-        $this->app->bind('InetStudio\Menu\Contracts\Http\Responses\Back\Menus\FormResponseContract', 'InetStudio\Menu\Http\Responses\Back\Menus\FormResponse');
-        $this->app->bind('InetStudio\Menu\Contracts\Http\Responses\Back\Menus\IndexResponseContract', 'InetStudio\Menu\Http\Responses\Back\Menus\IndexResponse');
-        $this->app->bind('InetStudio\Menu\Contracts\Http\Responses\Back\Menus\SaveResponseContract', 'InetStudio\Menu\Http\Responses\Back\Menus\SaveResponse');
-
-        // Services
-        $this->app->bind('InetStudio\Menu\Contracts\Services\Back\Menus\MenusDataTableServiceContract', 'InetStudio\Menu\Services\Back\Menus\MenusDataTableService');
-        $this->app->bind('InetStudio\Menu\Contracts\Services\Back\MenuItems\MenuItemsServiceContract', 'InetStudio\Menu\Services\Back\MenuItems\MenuItemsService');
-        $this->app->bind('InetStudio\Menu\Contracts\Services\Back\Menus\MenusObserverServiceContract', 'InetStudio\Menu\Services\Back\Menus\MenusObserverService');
-        $this->app->bind('InetStudio\Menu\Contracts\Services\Back\MenuItems\MenuItemsObserverServiceContract', 'InetStudio\Menu\Services\Back\MenuItems\MenuItemsObserverService');
-        $this->app->bind('InetStudio\Menu\Contracts\Services\Back\Menus\MenusServiceContract', 'InetStudio\Menu\Services\Back\Menus\MenusService');
-        $this->app->bind('InetStudio\Menu\Contracts\Services\Front\MenusServiceContract', 'InetStudio\Menu\Services\Front\MenusService');
-
-        // Transformers
-        $this->app->bind('InetStudio\Menu\Contracts\Transformers\Back\MenuTransformerContract', 'InetStudio\Menu\Transformers\Back\MenuTransformer');
-        $this->app->bind('InetStudio\Menu\Contracts\Transformers\Back\PrepareMenuItemsTransformerContract', 'InetStudio\Menu\Transformers\Back\PrepareMenuItemsTransformer');
-        $this->app->bind('InetStudio\Menu\Contracts\Transformers\Back\TreeTransformerContract', 'InetStudio\Menu\Transformers\Back\TreeTransformer');
-        $this->app->bind('InetStudio\Menu\Contracts\Transformers\Front\TreeTransformerContract', 'InetStudio\Menu\Transformers\Front\TreeTransformer');
     }
 }
